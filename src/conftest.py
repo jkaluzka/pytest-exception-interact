d4e18a5d35822acf7c365734449a97fffc3516b9
Jakub Kaluzka <jakub.kaluzka@astrazeneca.com>
2021-12-23 12:48:30 +0100

    feat: Taking a browser state snapshot in case of error.

--- src/conftest.py
+++ src/conftest.py
@@ -1,6 +1,8 @@
 """
 pytest configuration file
 """
+from pathlib import Path
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -23,3 +25,23 @@ def chrome_browser() -> WebDriver:
 def search_page(chrome_browser: WebDriver) -> WebDriver:
     chrome_browser.get("https://duckduckgo.com")
     return chrome_browser
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_exception_interact(node, call, report):
+    """
+    Overrides the original hook to save browser state
+    in form of a screenshot into the "./errors" directory
+    """
+    web_driver = None
+    for fixture_name in node.fixturenames:
+        web_driver = node.funcargs[fixture_name]
+        if isinstance(web_driver, WebDriver):
+            break
+    if not web_driver:
+        yield
+
+    _path = "./errors"
+    Path(_path).mkdir(parents=True, exist_ok=True)
+    name = "-".join(node.nodeid.split("::")[-2:])
+    web_driver.save_screenshot(f"{_path}/error_{name}.png")
